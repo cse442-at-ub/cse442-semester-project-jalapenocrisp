572505129c4d5531517590b60ee0e1c4d4fa1a78
Syed <syedrehm@buffalo.edu>
2020-02-18 08:58:30 -0500

    changed style of log in and signup

---
+++ login.js
@@ -0,0 +1,9 @@
+function log_me_in(){
+    // var log_in_button = Document.getElementById("log_in_button")
+    window.open("tutorprofile.html", "_self");
+    //
+}
+
+function goToLogIn(){
+    window.open("login.html", "_self");
+}
