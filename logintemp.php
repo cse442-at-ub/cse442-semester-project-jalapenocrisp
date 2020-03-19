ac777361586ce1b7017a590c57b4f51a66df050a
Nekesa Mercy <kesamercy@gmail.com>
2020-03-19 11:53:10 -0400

    renamed variables to match db variables in tmep login file

--- logintemp.php
+++ logintemp.php
@@ -3,27 +3,27 @@
    session_start();
    
    if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
+      // username and paswd sent from form 
       
       $myemail = mysqli_real_escape_string($db,$_POST['email']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['paswd']); 
+      $mypaswd = mysqli_real_escape_string($db,$_POST['paswd']); 
       
-      $sql = "SELECT id FROM tutors WHERE email = '$myusername' and paswd = '$mypassword'";
+      $sql = "SELECT id FROM tutors WHERE email = '$myemail' and paswd = '$mypaswd'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
       
       $count = mysqli_num_rows($result);
       
-      // If result matched $myusername and $mypassword, table row must be 1 row
+      // If result matched $myusername and $mypaswd, table row must be 1 row
 		
       if($count == 1) {
          session_register("myemail");
-         $_SESSION['login_user'] = $myusername;
+         $_SESSION['login_user'] = $myemail;
          
          header("location: welcome.php");
       }else {
-         $error = "Your Login Name or Password is invalid";
+         $error = "Your Login Name or paswd is invalid";
       }
    }
 ?>
@@ -59,7 +59,7 @@
                
                <form action = "" method = "post">
                   <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
-                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <label>paswd  :</label><input type = "paswd" name = "paswd" class = "box" /><br/><br />
                   <input type = "submit" value = " Submit "/><br />
                </form>
                
