688a213ea1d27a0788ca19d07f1b237fdefab788
Nekesa Mercy <kesamercy@gmail.com>
2020-03-19 11:36:01 -0400

    added new pages to test alternative login authentication method

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'tethys.cse.buffalo.edu');
+   define('DB_USERNAME', 'jenniech');
+   define('DB_PASSWORD', '50041501');
+   define('DB_DATABASE', 'cse442_542_2020_spring_teami_db');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
