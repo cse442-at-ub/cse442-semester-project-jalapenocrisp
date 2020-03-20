e9c2f2b792b8b201e3ca0f8aaa6918db2f0cb7e7
jechepenuk <jechepenuk@gmail.com>
2020-03-20 09:26:15 -0400

    issue 38, edit login to route to php file

--- index.js
+++ index.js
@@ -20,11 +20,6 @@ function loadPage(suburl, data, callback, postcallback) {
     });
 }
 
-function log_me_in(){
-    // var log_in_button = Document.getElementById("log_in_button")
-    window.open("./tutorprofile.html", "_self");
-    //./tutorprofile.html
-}
 
 function goToLogIn(){
     window.open("login.html", "_self");
