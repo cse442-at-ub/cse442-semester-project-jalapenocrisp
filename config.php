d49684a821deaf6b1aac750c1bca94dc371a4a53
Nekesa Mercy <kesamercy@gmail.com>
2020-03-19 20:34:38 -0400

    deleted temp files and implemented working functionality for login authentication using login.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'tethys.cse.buffalo.edu');
-   define('DB_USERNAME', 'nekesame');
-   define('DB_PASSWORD', '50278839');
-   define('DB_DATABASE', 'cse442_542_2020_spring_teami_db');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
