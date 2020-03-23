37f9cdfe72c23584bd7858571596de00956a113a
Syed Rehman <syedrehm@buffalo.edu>
2020-03-23 12:15:53 -0400

    there was an extra } on tutor_signup.php

--- tutor_signup.php
+++ tutor_signup.php
@@ -118,12 +118,11 @@
                         $stmt->bind_param("sssssss", $fname, $lname, $email, $phone, $title, $courses, $pass);
                         $stmt->execute();
                       
-                        ?>
-                        window.open("login.php", "self");
-                    }
-                    else{ 
-                        alert('Password must be 8 to 15 characters long and have at least 1 uppercase and lowercase letter, 1 number, and 1 special character.' )
-                    }
+                    ?>
+                    window.open("login.php", "self");
+                }
+                else{ 
+                    alert('Password must be 8 to 15 characters long and have at least 1 uppercase and lowercase letter, 1 number, and 1 special character.' )
                 }  
             } 
         }
