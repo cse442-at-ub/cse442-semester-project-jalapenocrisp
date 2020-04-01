8248e751659631734771a19977d60d570185cfbc
jechepenuk <jechepenuk@gmail.com>
2020-04-01 16:50:31 -0400

    issue89 changing navbar links, button links, styling dropdowns to match

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ if(count($_POST)>0) {
 	$result = mysqli_query($conn,"SELECT * FROM tutors WHERE email='" . $_POST["email"] . "' and paswd = '". $_POST["paswd"]."'");
 	$count  = mysqli_num_rows($result);
 	if($count==0) {
-		$message = "Invalid email or paswd!";
+		$message = "Invalid email or password!";
 	} else {
         $row = mysqli_fetch_array($result);
         $message = "You are successfully authenticated!";
@@ -28,10 +28,11 @@ if(count($_POST)>0) {
             <ul>
                 <!-- the line of code commented below is important when we upload the work on a server. for now, i'm using an alternative below -->
                 <!-- <li><a href="javascript:loadPage('./login.php')">login</a> </li> -->
-                <li><a href="./login.php">login</a> </li>
                 <li>
                     <a href="./index.html">home</a> </li>
-                <li>create account</li>
+                <li>
+                    <a href="./tutor-signup.php">create account</a> </li>
+
             </ul>
         </div>
 
@@ -40,7 +41,7 @@ if(count($_POST)>0) {
         </div>
     </div>
 
-    <button class="selectButton" onclick="window.location.href = './tutor_signup.html';">Not Registered? Sign Up
+    <button class="selectButton" onclick="window.location.href = './tutor-signup.php';">Not Registered? Sign Up
 
         Here.</button>
 
@@ -58,7 +59,7 @@ if(count($_POST)>0) {
         </div> 
 
             <label for="email">User Email</label>
-            <input class="log_in_input" type="text" id="email" name="email" placeholder="Enter @buffalo.edu email...">
+            <input class="log_in_input" type="text" id="email" name="email" placeholder="Email">
 
             <label for="password">Password</label>
             <input class="log_in_input" type="password" id="password" name="paswd">
