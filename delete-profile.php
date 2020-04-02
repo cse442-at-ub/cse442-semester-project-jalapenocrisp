315da18a82027dc05cc1d0ac64e1f46fb86d4afe
jechepenuk <jechepenuk@gmail.com>
2020-04-02 00:01:30 -0400

    issue21 sql statements to assist deletion

--- delete-profile.php
+++ delete-profile.php
@@ -1,2 +1,11 @@
 <?php
 include_once "access-db.php";
+$uid=$_GET['user_id'];
+$sql  =  "DELETE FROM tutors WHERE id=$uid";
+$stmt= $conn->prepare($sql);
+$stmt->bind_param("i", $uid);
+$stmt->execute();
+
+header('Location: ./index.html');
+echo "Profile deleted successfully";
+?>
