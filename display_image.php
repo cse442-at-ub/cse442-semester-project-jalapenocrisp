3b60a9510b677495d507a9079b864325807e4b7c
Nekesa Mercy <kesamercy@gmail.com>
2020-04-06 16:43:59 -0400

    included html file to enable the upload of the prof pic to be saved to the db

--- display_image.php
+++ display_image.php
@@ -1,3 +1,11 @@
+
+<?php
+
+$getname = $_GET[' your_imagename '];
+
+echo "< img src = fetch_image.php?name=".$getname." width=200 height=200 >";
+
+?>
 <html>
 <body>
 		
@@ -9,11 +17,3 @@
 </body>
 </html>
 
-
-<?php
-
-$getname = $_GET[' your_imagename '];
-
-echo "< img src = fetch_image.php?name=".$getname." width=200 height=200 >";
-
-?>
