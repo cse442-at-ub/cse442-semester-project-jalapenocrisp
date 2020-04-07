5d67074049a6e7b4f139d076f07cceb65e42fa85
Nekesa Mercy <kesamercy@gmail.com>
2020-04-06 23:03:32 -0400

    fixed syntax error with a file name for the action variable in the form

--- temp-upload.php
+++ temp-upload.php
@@ -47,7 +47,7 @@ if ($uploadOk == 0) {
 <html>
 <body>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
+<form action="temp-upload.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
