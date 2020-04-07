87bbb8f37e1a45a660302c4fb4ee9ff2e7efaac2
Nekesa Mercy <kesamercy@gmail.com>
2020-04-07 16:25:58 -0400

    added a circle frame to the profile picture

--- display_image.php
+++ display_image.php
@@ -1,6 +1,17 @@
 
 <html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css" />
+    <script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
 <body>
+
+<!-- the next steps
+1. put this picture in a circle frame for display  
+2. figure out how and modify the code to display the picture based on the user who is logged in at the time. use user_id
+3. move this code to the tutor prof page and re-design the prof. pic part to be a circle and this image showing in the circle
+4. you only need the getdata php code and the display image code in the tutor prof oage with the tags ** look into how to implement this part anyway!-->
 		
 <form method="GET" action="display_image.php" >
  <input type="file" name="your_imagename">
@@ -16,7 +27,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['imagetmp'] ).'"/>';
+        echo '<img class="profilePicture" src="data:image/jpeg;base64,'.base64_encode( $row['imagetmp'] ).'"/>';
     
        // echo "<br> imagename: ". $row["imagename"]. " - pic: ". $row["imagetmp"]. "<br>";
     }
