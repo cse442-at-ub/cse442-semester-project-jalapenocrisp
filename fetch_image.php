9ede3f93dae30865d7be573db338295a069c83df
Nekesa Mercy <kesamercy@gmail.com>
2020-04-07 00:05:06 -0400

    no new changes. debuggin statement

--- fetch_image.php
+++ fetch_image.php
@@ -1,22 +1,16 @@
 <?php
-$servername = "tethys.cse.buffalo.edu";
-$username = "jenniech";
-$password = "50041501";
-$db = "cse442_542_2020_spring_teami_db";
-// Create connection
-$conn = new mysqli($servername, $username, $password, $db);
-
+include_once 'access-db.php';
 
 $name=$_GET['name'];
 
-$select_image="select * from image_table where imagename='$name'";
+$select_image="select imagetmp from image_table where imagename='$name'";
 
 $var=mysql_query($select_image);
 
 if($row=mysql_fetch_array($var))
 {
  $image_name=$row["imagename"];
- $image_content=$row["imagecontent"];
+ $image_content=$row["imagetmp"];
 }
-echo $image;
+echo $select_image;
 ?>
