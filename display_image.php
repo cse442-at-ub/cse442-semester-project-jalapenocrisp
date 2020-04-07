015679224c7fe41aa2abea62563ff8c0f7fb01d0
Nekesa Mercy <kesamercy@gmail.com>
2020-04-07 16:29:04 -0400

    no new changes. debugging

--- display_image.php
+++ display_image.php
@@ -1,11 +1,11 @@
 
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="style.css" />
+
     <script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    
 </head>
 <body>
 
