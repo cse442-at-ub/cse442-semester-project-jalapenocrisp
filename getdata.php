ccf55abb0ba4ec479766271042df89eef8fa90f6
Nekesa Mercy <kesamercy@gmail.com>
2020-04-06 23:29:40 -0400

    no new changes. debuggin statement

--- getdata.php
+++ getdata.php
@@ -7,7 +7,7 @@ $imagename=$_FILES["myimage"]["name"];
 $imagetmp=addslashes (file_get_contents($_FILES['myimage']['tmp_name']));
 
 //Insert the image name and image content in image_table
-$insert_image="INSERT INTO image_table VALUES('$imagetmp','$imagename')";
+$insert_image="INSERT INTO image_table (imagetmp, imagename) VALUES('$imagetmp','$imagename')";
 
 mysql_query($conn, $insert_image);
 
