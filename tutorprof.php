13d80702c8ac0aeead45eaf1e1aec66b512025e5
sedrehman <syedrehm@buffalo.edu>
2020-04-08 14:03:14 -0400

    problem in tutorProf not pulling tutor -fixed

--- tutorprof.php
+++ tutorprof.php
@@ -1,6 +1,7 @@
+  
 <?php
 include_once "access-db.php";
-$result = mysqli_query($conn,"SELECT * FROM tutors WHERE user_id='" . $_GET['tutor_id'] . "'");
+$result = mysqli_query($conn,"SELECT * FROM tutors WHERE user_id='" . $_GET['user_id'] . "'");
 $row = mysqli_fetch_array($result);
 ?>
 <!DOCTYPE html>
@@ -25,12 +26,6 @@ $row = mysqli_fetch_array($result);
 
                 <!-- the line of code commented below is important when we upload the work on a server. for now, i'm using an alternative below -->
                 <!-- <li><a href="javascript:loadPage('./login.html')">login</a> </li> -->
-                <li><a class="navlink" href="./student-appts.php?user_id=<?php echo $_GET['user_id']; ?>">my appointments</a> </li>
-
-                <li><a class="navlink" href="./search.php">find a tutor</a> </li>
-
-                <li><a class="navlink" href="./studentprof.php?user_id=<?php echo $_GET['user_id']; ?>">profile</a> </li>
-
 
                 <li><a class="navlink" href="./index.html">logout</a> </li>
 
@@ -42,16 +37,11 @@ $row = mysqli_fetch_array($result);
         </div>
 
     </div>
-    <hr class="hr-navbar">
-
-    <button class="calendarView" onclick="window.location.href = './tutor-calendar-view-student.php?user_id=<?php echo $_GET['user_id']; ?>&tutor_id=<?php echo $_GET['tutor_id']; ?>'">Make an Appointment</button>
+    <button class="calendarView" onclick="window.location.href = './tutorCalendarView.php?user_id=<?php echo $row['user_id']; ?>';">Calendar View</button>
 
     <h1 class="welcome-page-title"></h1>
     <table class="info">
 
-    <?php
-    
-    ?>
     <tr><td>Name: </td><td><?php echo $row["fname"]; ?> <?php echo $row["lname"]; ?></td></tr>
     <tr><td>Phone Number: </td><td><?php echo $row["phone"]; ?></td></tr>
     <tr><td>Title: </td><td><?php echo $row["title"]; ?></td></tr>
@@ -60,6 +50,9 @@ $row = mysqli_fetch_array($result);
     <tr><td class="score" title="The number of tutoring hours this tutor has completed.">Score: </td><td><?php echo $row["score"]; ?></td></tr>
     
     </table>
+    <button class="selectButton" onclick="window.location.href ='./update-tutor-profile.php?user_id=<?php echo $row['user_id']; ?>';">Edit Information</button>  
+    
+    <button class="delButton" onclick="window.location.href ='./delete-profile.php?user_id=<?php echo $row['user_id']; ?>';">Delete Profile</button> 
     <br><br><br>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
