a61d396afe337286acf97804d07747fbb51e67d2
jechepenuk <jechepenuk@gmail.com>
2020-04-10 10:23:47 -0400

    fixing navbar links

--- tutorCalendarView.php
+++ tutorCalendarView.php
@@ -38,6 +38,8 @@
 
                 <!-- the line of code commented below is important when we upload the work on a server. for now, i'm using an alternative below -->
                 <!-- <li><a href="javascript:loadPage('./login.php')">login</a> </li> -->
+                <li><a class="navlink" href="./tutor-appts.php?user_id=<?php echo $_GET['user_id']; ?>">appointments</a> </li>
+                <li><a class="navlink" href="./tutorprof.php?user_id=<?php echo $_GET['user_id']; ?>">profile</a> </li>
                 <li><a class="navlink" href="./index.html">logout</a> </li>
 
             </ul>
@@ -137,9 +139,6 @@
         </table>
     </form>
     
-    <form style="width: 100%; text-align: center; ">
-        <button id="popup_open" class="add-or-edit-button" type="submit"> My Appointments </button>
-    </form>
     <script src="index.js"></script>
     
 </body>
