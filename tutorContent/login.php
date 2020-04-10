190d24e240c29a13568424437157fa97ef7de0f9
jechepenuk <jechepenuk@gmail.com>
2020-04-10 12:08:00 -0400

    fixing links to include folder header

--- tutorContent/login.php
+++ tutorContent/login.php
@@ -21,7 +21,7 @@ if(count($_POST)>0) {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="stylesheet" type="text/css" href="../style.css" />
     <title>UB Tutoring -Log In</title>
 </head>
 
