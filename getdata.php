1e0760d2066228c1d2dc52ec25317ce70d2b9a87
Nekesa Mercy <kesamercy@gmail.com>
2020-04-11 10:53:10 -0400

    debugging update img to db

--- getdata.php
+++ getdata.php
@@ -11,7 +11,7 @@ $imagetmp=addslashes (file_get_contents($_FILES['myimage']['tmp_name']));
 //Insert the image name and image content in image_table
 //$insert_image="INSERT INTO tutors (user_image, img_name) VALUES('$imagetmp','$imagename')";
 
-$insert_image = "UPDATE tutors SET user_image='$imagetmp', img_name='$imagename' WHERE user_id= $userid " ;
+$insert_image = "UPDATE tutors SET user_image='$imagetmp', img_name='$imagename' WHERE user_id= '" . $_GET['user_id'] . "' " ;
 
 if ($conn->query($insert_image) === TRUE) {
     echo "New record created successfully. Naviagate back to see your image";
