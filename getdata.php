834adee5cbba1ec16748ee68e5323bbd7931ed23
Nekesa Mercy <kesamercy@gmail.com>
2020-04-11 11:22:11 -0400

    debugging update db

--- getdata.php
+++ getdata.php
@@ -1,17 +1,19 @@
 <?php
 include_once 'access-db.php';
 
-$userid = mysqli_query($conn,"SELECT * FROM tutors WHERE user_id='" . $_GET['user_id'] . "'");
-
+// $result = mysqli_query($conn,"SELECT * FROM tutors WHERE user_id='" . $_GET['user_id'] . "'");
+// $sql = "SELECT user_image FROM tutors WHERE user_id='" . $_GET['user_id'] . "'";
+// $result
 $imagename=$_FILES["myimage"]["name"]; 
 
 //Get the content of the image and then add slashes to it 
 $imagetmp=addslashes (file_get_contents($_FILES['myimage']['tmp_name']));
 
+$userid = $_POST['user_id'];
 //Insert the image name and image content in image_table
 //$insert_image="INSERT INTO tutors (user_image, img_name) VALUES('$imagetmp','$imagename')";
 
-$insert_image = "UPDATE tutors SET user_image='$imagetmp', img_name='$imagename' WHERE user_id= '" . $_GET['user_id'] . "' " ;
+$insert_image = "UPDATE tutors SET user_image='$imagetmp', img_name='$imagename' WHERE user_id= '$userid' " ;
 
 if ($conn->query($insert_image) === TRUE) {
     echo "New record created successfully. Naviagate back to see your image";
