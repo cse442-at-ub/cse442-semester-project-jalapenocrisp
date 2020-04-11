5cb5bb68b2c3d950ee7787179187889abb199811
Nekesa Mercy <kesamercy@gmail.com>
2020-04-11 14:18:40 -0400

    debuggingv8

--- tutorprof.php
+++ tutorprof.php
@@ -2,31 +2,6 @@
 include_once "access-db.php";
 $result = mysqli_query($conn,"SELECT * FROM tutors WHERE user_id='" . $_GET['user_id'] . "'");
 $row = mysqli_fetch_array($result);
-
-if(count($_POST)>0) {
-    $userid=$_GET['user_id'];
-    //$userid=1;
-    $imagename=$_FILES["myimage"]["name"]; 
-    //Get the content of the image and then add slashes to it 
-    $imagetmp=addslashes (_POSTfile_get_contents($_FILES['myimage']['tmp_name']));
-
-    //Insert the image name and image content in image_table
-    //$insert_image="INSERT INTO tutors (user_image, img_name) VALUES('$imagetmp','$imagename')";
-
-    $insert_image = "UPDATE tutors SET user_image='$imagetmp', img_name='$imagename' WHERE user_id='" . $_GET['user_id'] . "'" ;
-
-    if ($conn->query($insert_image) === TRUE) {
-        echo "this is the " .$userid.".";
-        echo "new";
-        echo "New record created successfully. Naviagate back to see your image";
-        echo "";
-        echo '<p><a href="tutorprof.php">Back to tutorprof.php</a>';
-    } else {
-        echo "Error: " . $insert_image . "<br>" . $conn->error;
-    }
-
-    $conn->close();
-}
 ?>
 
 <!DOCTYPE html>
@@ -67,6 +42,7 @@ if(count($_POST)>0) {
     <?php
      echo '<img class="profilePicture" src="data:image/jpeg;base64,'.base64_encode( $row['user_image'] ).'"/>';
 
+
     ?>
 
 
@@ -97,3 +73,32 @@ if(count($_POST)>0) {
 </body>
 
 </html>
+
+<?php
+include_once 'access-db.php';
+
+if(count($_POST)>0) {
+    $userid=$_GET['user_id'];
+    //$userid=1;
+    $imagename=$_FILES["myimage"]["name"]; 
+    //Get the content of the image and then add slashes to it 
+    $imagetmp=addslashes (_POSTfile_get_contents($_FILES['myimage']['tmp_name']));
+
+    //Insert the image name and image content in image_table
+    //$insert_image="INSERT INTO tutors (user_image, img_name) VALUES('$imagetmp','$imagename')";
+
+    $insert_image = "UPDATE tutors SET user_image='$imagetmp', img_name='$imagename' WHERE user_id='" . $_GET['user_id'] . "'" ;
+
+    if ($conn->query($insert_image) === TRUE) {
+        echo "this is the " .$userid.".";
+        echo "new";
+        echo "New record created successfully. Naviagate back to see your image";
+        echo "";
+        echo '<p><a href="tutorprof.php">Back to tutorprof.php</a>';
+    } else {
+        echo "Error: " . $insert_image . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
