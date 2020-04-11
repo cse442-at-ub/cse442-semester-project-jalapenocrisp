87724c9b4eb82ea8956b42dad7f4bd622a61437e
Nekesa Mercy <kesamercy@gmail.com>
2020-04-11 13:50:07 -0400

    debuggingv5

--- getdata.php
+++ getdata.php
@@ -2,16 +2,16 @@
 include_once 'access-db.php';
 
 if(count($_POST)>0) {
-    //$userid=$_POST['user_id'];
-    $userid=1;
+    $userid=$_GET['user_id'];
+    //$userid=1;
     $imagename=$_FILES["myimage"]["name"]; 
     //Get the content of the image and then add slashes to it 
-    $imagetmp=addslashes (file_get_contents($_FILES['myimage']['tmp_name']));
+    $imagetmp=addslashes (_POSTfile_get_contents($_FILES['myimage']['tmp_name']));
 
     //Insert the image name and image content in image_table
     //$insert_image="INSERT INTO tutors (user_image, img_name) VALUES('$imagetmp','$imagename')";
 
-    $insert_image = "UPDATE tutors SET user_image='$imagetmp', img_name='$imagename' WHERE user_id=1 " ;
+    $insert_image = "UPDATE tutors SET user_image='$imagetmp', img_name='$imagename' WHERE user_id='" . $_GET['user_id'] . "'" ;
 
     if ($conn->query($insert_image) === TRUE) {
         echo "this is the " .$userid.".";
