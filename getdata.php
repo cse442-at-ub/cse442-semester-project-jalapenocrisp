d568a5bfb4e7be7c5e94b11dc27fd24c0d76f44c
Nekesa Mercy <kesamercy@gmail.com>
2020-04-11 09:43:14 -0400

    debugging routing for the image retrival from db

--- getdata.php
+++ getdata.php
@@ -7,7 +7,9 @@ $imagename=$_FILES["myimage"]["name"];
 $imagetmp=addslashes (file_get_contents($_FILES['myimage']['tmp_name']));
 
 //Insert the image name and image content in image_table
-$insert_image="INSERT INTO tutors (user_image, img_name) VALUES('$imagetmp','$imagename') WHERE user_id=1";
+//$insert_image="INSERT INTO tutors (user_image, img_name) VALUES('$imagetmp','$imagename')";
+
+$insert_image = "UPDATE tutors SET user_image='$imagetmp', img_name='$imagename' WHERE id = 1";
 
 if ($conn->query($insert_image) === TRUE) {
     echo "New record created successfully. Naviagate back to see your image";
@@ -18,3 +20,13 @@ if ($conn->query($insert_image) === TRUE) {
 $conn->close();
 
 ?>
+
+
+
+<!-- 
+1. have 2 pages, if they are a new user, use the insert
+2. if they are an old user, use the update query stamenet 
+
+1. have another pa
+
+ -->
