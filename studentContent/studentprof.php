b64679cfc25610157e74955907a5cedd35cbb5a0
Jennie Chepenuk <36555776+jechepenuk@users.noreply.github.com>
2020-04-13 18:39:32 -0400

    fixing merge issues with db retrieval

--- studentContent/studentprof.php
+++ studentContent/studentprof.php
@@ -2,6 +2,8 @@
 include_once "access-db.php";
 $result = mysqli_query($conn,"SELECT * FROM students WHERE user_id='" . $_GET['user_id'] . "'");
 $row = mysqli_fetch_array($result);
+$progress= mysqli_query($conn,"SELECT * FROM progress WHERE student_id='" . $_GET['user_id'] . "'");
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
