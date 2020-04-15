ed71c513007f642bae0920a9e32867044d698bd9
jechepenuk <jechepenuk@gmail.com>
2020-04-15 17:55:16 -0400

    issue154 adding default profile picture to balance mobile view

--- studentContent/tutorprof-student.php
+++ studentContent/tutorprof-student.php
@@ -71,7 +71,9 @@ $progress= mysqli_query($conn,"SELECT * FROM progress WHERE student_id='" . $_GE
 
     if ($row['user_image']){
      echo '<img class="profilePicture" src="data:image/jpeg;base64,'. $row['user_image'] .'"/>';
-    }
+    }else{
+        echo '<img class="profilePicture" src="user-default.jpg" alt="you">';
+       }
     ?>    
 
     <h1 class="welcome-page-title"></h1>
