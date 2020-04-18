692f0641c7adb9fa799f9bdc900622fa2ab53c43
Jennie Chepenuk <36555776+jechepenuk@users.noreply.github.com>
2020-04-18 12:52:14 -0400

    bugfix tmobile #s

--- studentContent/verify-text-student.php
+++ studentContent/verify-text-student.php
@@ -62,16 +62,16 @@
         $to.='@';
         $to.=$emaillink;
         $code= strval(mt_rand(100000, 999999));
-        $message="Your verification code is ";
-        $message.=$code;
+        $message="<p>Your verification code is </p>";
+        $message.="<p> .$code. </p>";
         $from="no-reply@buffalo.com";
         $headers  = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type: text/plain; charset=iso-8859-1" . "\r\n";
+        $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
         $headers .= "From: ". $from. "\r\n";
         $headers .= "Reply-To: ". $from. "\r\n";
         $headers .= "X-Mailer: PHP/" . phpversion();
         $headers .= "X-Priority: 1" . "\r\n";
-        mail( '5719929514@tmomail.net', '', $message );
+        mail( $to, '', $message );
         mysqli_query($conn,"UPDATE students SET vcode='" . $code  . "' WHERE user_id='" . $_GET['user_id'] . "'"); 
     }    
         
