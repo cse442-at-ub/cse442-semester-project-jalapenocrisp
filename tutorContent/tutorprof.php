50ff497c5a776cd301c0d97a4e6a38ec3156bb22
Elwyn Jones <elwynjon@buffalo.edu>
2020-04-20 19:31:24 -0400

    Issue#147 edited tutor profile to display or add zoom link fix attempt2

--- tutorContent/tutorprof.php
+++ tutorContent/tutorprof.php
@@ -48,14 +48,7 @@ $row = mysqli_fetch_array($result);
     
     <button class="calendarView" onclick="window.location.href = './change-photo.php?user_id=<?php echo $row['user_id']; ?>';">Add/Edit Photo</button>
 
-    <?php
-    if($row['zoom_link'] != NULL){
-	
-	<button class="calendarView" onclick="window.location.href = './add-zoom.php?user_id=<?php echo $row['user_id']; ?>';">Edit Zoom Link</button>
-    }else{
-	<button class="calendarView" onclick="window.location.href = './add-zoom.php?user_id=<?php echo $row['user_id']; ?>';">Add Zoom Link</button>
-    }
-    ?>    
+    
 
 
     <h1 class="welcome-page-title"></h1>
