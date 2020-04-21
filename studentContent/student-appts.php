b57bcd26e32a6ec3adca6bc6850cf8a9652ad895
sedrehman <syedrehm@buffalo.edu>
2020-04-21 16:26:57 -0400

    issue#149 adding notification -- testing it--

--- studentContent/student-appts.php
+++ studentContent/student-appts.php
@@ -53,7 +53,9 @@ echo "$num_of_grades \n";
 
 
                 <li><a class="navlink" href="./student-add-exam.php?user_id=<?php echo $row['user_id']; ?>">add grades<?php
-                    
+                    if($num_of_grades > 0){
+                        echo"<span style=\"border-radius: 50%; background-color: red; color: white;\">$num_of_grades</span>";
+                    }
                 ?></a> </li>
                 
                 <li><a class="navlink" href="./search.php?user_id=<?php echo $row['user_id']; ?>">find a tutor</a> </li>
