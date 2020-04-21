b999519b52d1e46cc8148e17e4466c05f01bba84
Elwyn Jones <elwynjon@buffalo.edu>
2020-04-20 20:39:58 -0400

    Issue#147 add zoomlink page page-break fix

--- tutorContent/add-zoom.php
+++ tutorContent/add-zoom.php
@@ -3,19 +3,7 @@
 $message="";
 include_once "access-db.php";
     
-if(count($_POST)>0){
 
-    $zoomLink = $_POST['zoomLink'];
-
-    if(empty($zoomLink)){
-	$message="Please enter your Zoom Link"; 
-    }else if(strlen($zoomLink)!=22 || strlen($zoomLink)!=23){
-    	  $message="PLease enter ";
-    }else{    
-     	      mysqli_query($conn,"UPDATE tutors SET zoom_link= $zoom WHERE user_id='" . $_GET['user_id'] . "'");
-	      $message = "Link added successfully!";
-     	      header('Location: ./studentprof.php?user_id=' . $_GET["user_id"]);  
-     }
 
      $result1 = mysqli_query($conn,"SELECT * FROM tutorstudents WHERE user_id='" . $_GET["user_id"] . "'");
     $row=mysqli_fetch_array($result1);
@@ -37,9 +25,11 @@ if(count($_POST)>0){
 <body>
 
 	<input type="button" onclick="window.location='https://zoom.us/signin'" value="Go to Zoom Sign in"/>	 	 
-
+        <br>
+	 <p>Open the link above in a new tab.  \nThen, sign into your Zoom account and \ncopy your meeting link (found below you Personal Meeting ID). \nReturn to this page and paste it in the textbox below.</p>
+	 <br>
 	<form method="post" action="">
-	    <p>Open the link above in a new tab.  \nThen, sign into your Zoom account and \ncopy your meeting link (found below you Personal Meeting ID). \nReturn to this page and paste it in the textbox below.</p>
+	   
 	    <br>  
 	    <input class="sign_up_input" type="text" id= "zoomLink" name="zoomLink" value="echo $row['zoom-link']">
             <input type="submit" id="tutor_zoom_submit" name="submit" value= "Submit"> 
