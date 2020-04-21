5cc5b3752f7aed4ca9b4a3c09ae8a880633edf98
Tre' Jones <50718266+TKTre@users.noreply.github.com>
2020-04-21 16:40:06 -0400

    Issue#147 zoom button display attmpt11

--- studentContent/tutorprof-student.php
+++ studentContent/tutorprof-student.php
@@ -80,9 +80,16 @@ $row = mysqli_fetch_array($result);
 
     <?php
     if(is_null($row['zoom_link']) == false){
-        echo '<button class="calendarView" onclick="window.open("https://www.google.com/",'_blank')">Go to This Tutors Zoom Meeting Room</button>';
+        echo '<button class="calendarView" onclick="window.open(,'_blank')">Go to This Tutors Zoom Meeting Room</button>';
     }
     ?>
+    
+    
+    <?php if ($row['zoom_link'] == NULL) : ?>
+    <button name="zoomButtn" onclick="window.open('<?php echo $row['zoom_link']?>','_blank')" download style="visibility: hidden">
+    <i aria-hidden="true"></i> 
+    Go to This Tutors Zoom Meeting Room</button>
+    <?php endif ?>
      
     
     <table class="info">
