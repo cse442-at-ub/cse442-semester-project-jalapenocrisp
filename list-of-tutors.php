994e405f0192a2612778324045e1495d2aba0b4a
Nekesa Mercy <kesamercy@gmail.com>
2020-04-27 18:50:23 -0400

    Update list-of-tutors.php
    deleted un unnecessary links

--- list-of-tutors.php
+++ list-of-tutors.php
@@ -29,15 +29,14 @@
 
                 <!-- the line of code commented below is important when we upload the work on a server. for now, i'm using an alternative below -->
                 <!-- <li><a href="javascript:loadPage('./login.php')">login</a> </li> -->
-                <li><a class="navlink" href="./tutorCalendarView.php?user_id=<?php echo $_GET['user_id']; ?>">set availability</a> </li>
-                <li><a class="navlink" href="./tutorprof.php?user_id=<?php echo $_GET['user_id']; ?>">profile</a> </li>
+                
                 <li><a class="navlink" href="../index.html">logout</a> </li>
 
             </ul>
         </div>
 
         <div class="logo">
-            <h2 class="logo"> <a href="../index.html">UBtutoring</a> </h2>
+            <h2 class="logo"> <a href="index.html">UBtutoring</a> </h2>
         </div>
 
     </div>
