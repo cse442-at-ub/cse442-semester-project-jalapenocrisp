764ca802b0a146ae553404ba2e02eacc85b836ba
Nekesa Mercy <kesamercy@gmail.com>
2020-04-30 13:26:08 -0400

    debugging

--- tutorContent/login.php
+++ tutorContent/login.php
@@ -72,10 +72,12 @@ if(count($_POST)>0) {
            
             <div class="modal-input">
             
-                <label for="email">User Email</label>
-                <input class="log_in_input" type="text" id="email" name="email" placeholder="Email">
+             <input type="text" id="email" name="email" placeholder="Email">
+                
             </div>
 
+            <label for="email">User Email</label>
+                <input class="log_in_input" type="text" id="email" name="email" placeholder="Email">
             <label for="password">Password</label>
             <input class="log_in_input" type="password" id="password" name="paswd">
             
