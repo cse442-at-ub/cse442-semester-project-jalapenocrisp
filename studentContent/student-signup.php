c8e643d76c504f65749e011f505bd19cafa5f566
Nekesa Mercy <kesamercy@gmail.com>
2020-05-01 14:16:01 -0400

    added google fonts to the student sign up page

--- studentContent/student-signup.php
+++ studentContent/student-signup.php
@@ -59,6 +59,10 @@
     <link rel="stylesheet" type="text/css" href="../style.css" />
     <script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@500&family=Noto+Serif:wght@700&family=Roboto+Slab:wght@900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow&family=Fredericka+the+Great&family=Noto+Serif&family=Roboto&display=swap" rel="stylesheet">
+    
     <title>UB Tutoring Service</title>
 </head>
 <body>
