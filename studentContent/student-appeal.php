08981324c5c252d2388e656b0413bbc4b343e250
sedrehman <syedrehm@buffalo.edu>
2020-05-02 23:07:49 -0400

    issue#183 created new page for student appeal and added a link on student login page

--- studentContent/student-appeal.php
+++ studentContent/student-appeal.php
@@ -20,7 +20,7 @@ if(count($_POST)>0) {
         if($num_of_complete >= $num_of_cancel){
             header('Location: ./student-appts.php?user_id=' .$var1);
         }else{
-            $message= " It seems you cancellation rate is too high <br> You're currently being banned. For more info <a href=\"student-appeal.php\"> contact us </a>";
+            $message= " It seems you cancellation rate is too high <br> You're currently being banned. For more info <a href=\"user-forgot-student.php\"> contact us </a>";
         }
 	}
 }
